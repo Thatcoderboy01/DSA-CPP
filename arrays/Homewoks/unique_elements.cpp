68a05fef1feff47f95d60b2e084d2c6947eed75a
Thatcoderboy01 <thatcoderboy01@gmail.com>
2026-04-03 09:06:24 +0530

    find the unique elements in array

--- arrays/Homewoks/unique_elements.cpp
+++ arrays/Homewoks/unique_elements.cpp
@@ -4,6 +4,19 @@ using namespace std;
 // 🔹 Q3: Print unique elements
 void printUnique(int arr[], int size){
     // TODO: print elements that appear only once
+
+    for(int i=0; i<size; i++){
+        int count = 0;
+
+        for(int j=0; j<size; j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
+        }
+        if(count == 1){
+            cout<< arr[i]<< " ";
+        }
+    }
 }
 
 int main(){

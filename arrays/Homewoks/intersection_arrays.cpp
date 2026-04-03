815c2e70f6efd6c45a544e0f48c2d65ac702a9d0
Thatcoderboy01 <thatcoderboy01@gmail.com>
2026-04-03 09:20:34 +0530

    Intersection of two arrays

--- arrays/Homewoks/intersection_arrays.cpp
+++ arrays/Homewoks/intersection_arrays.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // 🔹 Q4: Intersection of 2 arrays
 void intersection(int arr1[], int size1, int arr2[], int size2){
     // TODO: print common elements
+    vector<bool> visited(size2, false);
+
+    for(int i=0; i<size1; i++){
+        for(int j=0; j<size2; j++){
+            if(arr1[i] == arr2[j] && !visited[j]){
+                cout << arr1[i]<< " ";
+                visited[j] = true;
+                break;
+
+            }
+        }
+    }
+
 }
 
 int main(){

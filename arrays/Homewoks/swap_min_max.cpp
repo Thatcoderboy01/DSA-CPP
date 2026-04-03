fb55631f343e1b82f363f37c191562f0e27a3e60
Thatcoderboy01 <thatcoderboy01@gmail.com>
2026-04-03 08:57:33 +0530

    swap min and max in array

--- arrays/Homewoks/swap_min_max.cpp
+++ arrays/Homewoks/swap_min_max.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
 
 // 🔹 Q2: Swap min & max element
 void swapMinMax(int arr[], int size){
     // TODO: find min index & max index, then swap
+    int minIndex = 0;
+    int maxIndex = 0;
+
+    for (int i=0; i<size; i++){
+       if(arr[i] < arr[minIndex]){
+        minIndex = i;
+       }
+       if (arr[i] > arr[maxIndex]){
+        maxIndex = i;
+       }
+    }
+
+    swap (arr[minIndex], arr[maxIndex]);
+
 }
 
 int main(){
-    int arr[] = {4, 2, 7, 2, 9, 4, 5};
+    int arr[] = {4, 2, 7, 3, 9, 4, 5};
     int size = sizeof(arr) / sizeof(int);
 
     swapMinMax(arr, size);
